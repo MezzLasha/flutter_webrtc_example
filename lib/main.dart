90891cafcf1ac0a32c697faf61c00e639ca2e001
Lasha Mezvrishvili <64702427+MezzLasha@users.noreply.github.com>
2023-09-12 10:52:13 +0400

    add firebase options in firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:flutter_webrtc_example/firebase_options.dart';
 import 'package:flutter_webrtc_example/signaling.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
